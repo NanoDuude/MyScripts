847d958665b2236609b1f814f4df885045a84ae6
RoScripter <104586765+User123434@users.noreply.github.com>
2022-12-26 20:18:06 +0200

    DoorsTools.lua
    wow

--- DoorsTools.lua
+++ DoorsTools.lua
@@ -8,7 +8,7 @@ local Window = Rayfield:CreateWindow({
 	LoadingTitle = "Welcom XD",
 	LoadingSubtitle = "by BobloxHecker",
 	ConfigurationSaving = {
-		Enabled = true,
+		Enabled = false,
 		FolderName = nil,
 		FileName = "DoorsGui"
 	},
@@ -166,6 +166,20 @@ local Input = Tab:CreateInput({
 	end,
 })
 
+local Button = Tab:CreateButton({
+	Name = "Break Lights",
+	Callback = function()
+		require(game.ReplicatedStorage.ClientModules.Module_Events).breakLights(workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")], 1)
+	end,
+})
+
+local Button = Tab:CreateButton({
+	Name = "Flicker Lights",
+	Callback = function()
+		require(game.ReplicatedStorage.ClientModules.Module_Events).flickerLights(workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")], 1)
+	end,
+})
+
 local Tab = Window:CreateTab("Doors items", 4483362458) -- Title, Image
 
 local Button = Tab:CreateButton({
@@ -182,6 +196,120 @@ local Button = Tab:CreateButton({
 	end,
 })
 
+local Tab = Window:CreateTab("Keybinds", 4483362458) -- Title, Image
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Break Lights",
+	CurrentKeybind = "Q",
+	HoldToInteract = false,
+	Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		require(game.ReplicatedStorage.ClientModules.Module_Events).breakLights(workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")], 1)
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Flicker Lights",
+	CurrentKeybind = "R",
+	HoldToInteract = false,
+	Flag = "Keybind2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		require(game.ReplicatedStorage.ClientModules.Module_Events).flickerLights(workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")], 1)
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Summon Screech",
+	CurrentKeybind = "T",
+	HoldToInteract = false,
+	Flag = "Keybind2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		require(game.StarterGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech)(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+        workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Summon Halt",
+	CurrentKeybind = "Y",
+	HoldToInteract = false,
+	Flag = "Keybind3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		require(game.ReplicatedStorage.ClientModules.EntityModules.Shade).stuff(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+        workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Summon Glich",
+	CurrentKeybind = "U",
+	HoldToInteract = false,
+	Flag = "Keybind4", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		require(game.ReplicatedStorage.ClientModules.EntityModules.Glitch).stuff(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
+        workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Get Torch",
+	CurrentKeybind = "O",
+	HoldToInteract = false,
+	Flag = "Keybind5", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		game:GetObjects("rbxassetid://3499523244")[1].Parent = game.Players.LocalPlayer.Backpack
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Get Bloxy Cola",
+	CurrentKeybind = "L",
+	HoldToInteract = false,
+	Flag = "Keybind6", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		game:GetObjects("rbxassetid://66071183")[1].Parent = game.Players.LocalPlayer.Backpack
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Get Crucifix",
+	CurrentKeybind = "P",
+	HoldToInteract = false,
+	Flag = "Keybind7", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		game:GetObjects("rbxassetid://11712848097")[1].Parent = game.Players.LocalPlayer.Backpack
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Get Skeleton Key",
+	CurrentKeybind = "N",
+	HoldToInteract = false,
+	Flag = "Keybind8", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		game:GetObjects("rbxassetid://11538858761")[1].Parent = game.Players.LocalPlayer.Backpack
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Get Milk",
+	CurrentKeybind = "C",
+	HoldToInteract = false,
+	Flag = "Keybind8", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		game:GetObjects("rbxassetid://8466449126")[1].Parent = game.Players.LocalPlayer.Backpack
+	end,
+})
+
+local Keybind = Tab:CreateKeybind({
+	Name = "Get Pistol",
+	CurrentKeybind = "X",
+	HoldToInteract = false,
+	Flag = "Keybind8", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Keybind)
+		game:GetObjects("rbxassetid://8640678553")[1].Parent = game.Players.LocalPlayer.Backpack
+	end,
+})
 
 local Tab = Window:CreateTab("Settings", 4483362458) -- Title, Image
 
@@ -189,5 +317,21 @@ local Button = Tab:CreateButton({
 	Name = "Destroy GUI",
 	Callback = function()
 		Rayfield:Destroy()
+	end,
+})
+
+local Tab = Window:CreateTab("Credits", 4483362458) -- Title, Image
+
+local Label = Tab:CreateLabel("The script was made by w0ow22")
+
+local Button = Tab:CreateButton({
+	Name = "bruh",
+	Callback = function()
+		Instance.new("Sound", game.Workspace).Name = "FartSound"
+        game.Workspace:FindFirstChild("FartSound").SoundId = 4809574295
+        game.Workspace:FindFirstChild("FartSound").Playing = true
+        game.Workspace:FindFirstChild("FartSound").Looped = true
+        wait(2)
+        game.Players.LocalPlayer.Character.Humanoid.Health = 0
 	end,
 })
